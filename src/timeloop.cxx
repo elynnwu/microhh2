e05e9379528ad7ec05eec73f3080eb35d576864e
elynn <elynn510@gmail.com>
2018-12-17 17:16:56 -0800

    bracket

--- src/timeloop.cxx
+++ src/timeloop.cxx
@@ -43,14 +43,18 @@ Timeloop<TF>::Timeloop(Master& masterin, Grid<TF>& gridin, Fields<TF>& fieldsin,
 
     // obligatory parameters
     if (sim_mode == Sim_mode::Init)
+    {
         starttime = 0.;
         datetime.tm_year = 2018 - 1900;
         datetime.tm_yday = input.get_item<int>("time", "jday"  , "", 0);
+    }
     else
+    {
         starttime = input.get_item<double>("time", "starttime", "");
         datetime.tm_sec = starttime;
         datetime.tm_year = 2018 - 1900;
-        datetime.tm_yday = input.get_item<int>("time", "jday"  , "", 0);
+        datetime.tm_yday = input.get_item<int>("time", "jday"  , "", 0);        
+    }
 
     endtime  = input.get_item<double>("time", "endtime" , "");
     savetime = input.get_item<double>("time", "savetime", "");
