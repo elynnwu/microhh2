31282c3b97e20e87c1c2b782f0d678a221101d14
elynn <elynn510@gmail.com>
2019-01-23 16:13:44 -0800

    include memory

--- src/radiation.cxx
+++ src/radiation.cxx
@@ -19,7 +19,7 @@
  * You should have received a copy of the GNU General Public License
  * along with MicroHH.  If not, see <http://www.gnu.org/licenses/>.
  */
-
+#include <memory>
 #include <cstdio>
 #include <cmath>
 #include <algorithm>
